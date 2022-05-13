740c0b738220c239595c2e560447edaab2e22047
shohjahon-sohibov <sohibov.s@bk.ru>
2022-05-13 18:19:47 +0500

    change protocol to http from https

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 9000
-const SERVERLINK = 'https://urgazcarpettest-env.eba-xsqikehx.us-west-2.elasticbeanstalk.com/'
+const SERVERLINK = 'http://urgazcarpettest-env.eba-xsqikehx.us-west-2.elasticbeanstalk.com/'
 
 module.exports = {
     PORT,
