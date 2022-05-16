0ccd21398ca889b2a5e362f23987ba912e4bc7e2
shohjahon-sohibov <sohibov.s@bk.ru>
2022-05-16 13:28:36 +0500

    sasa

--- src/modules/products/products.js
+++ src/modules/products/products.js
@@ -1,12 +1,12 @@
 const { products } = require("../../model/model");
-const { SERVERLINK } = require('../../config')
+const { SERVERLINK } = require("../../config");
 const fs = require("fs");
 const path = require("path");
 
 module.exports = {
   GET_PRODUCTS: async (_, res) => {
     try {
-      const Products = await products.findAll()
+      const Products = await products.findAll();
       res.status(200).json(Products);
     } catch (error) {
       return res.status(500).json({ error: error.message });
@@ -26,14 +26,14 @@ module.exports = {
         rating,
         sell,
         isNew,
-        isTop
+        isTop,
       } = req.body;
 
       let imagesArr = [];
       const file = req.file;
       const imgUrl = `${SERVERLINK}public/uploads/products/${file.originalname}`;
       imagesArr.push(imgUrl);
-      const [poster] = imagesArr
+      const [poster] = imagesArr;
       await products.create({
         title,
         description,
@@ -71,7 +71,7 @@ module.exports = {
         rating,
         sell,
         isNew,
-        isTop
+        isTop,
       } = req.body;
 
       const findProductId = await products.findOne({
@@ -79,55 +79,54 @@ module.exports = {
           id,
         },
       });
-
       // if (findProductId.imageName != req.file.originalname) {
-        let imagesArr = [];
-        const file = req.file;
-        console.log(req.file, " 1111111111111111111111111111111");
-        const imgUrl = `${SERVERLINK}public/uploads/products/${file.originalname}`;
-        imagesArr.push(imgUrl);
-        const [poster] = imagesArr
+      let imagesArr = [];
+      const file = req.file;
+      console.log(req.file, " 1111111111111111111111111111111");
+      const imgUrl = `${SERVERLINK}public/uploads/products/${file.originalname}`;
+      imagesArr.push(imgUrl);
+      const [poster] = imagesArr;
 
-        if (findProductId) {
-          fs.unlinkSync(
-            path.resolve(
-              __dirname,
-              `../../../public/uploads/products/${findProductId.imageName}`
-            ),
-            (error) => {
-              res.status(500).json({ error: error.message });
-            }
-          );
+      if (findProductId) {
+        fs.unlinkSync(
+          path.resolve(
+            __dirname,
+            `../../../public/uploads/products/${findProductId.imageName}`
+          ),
+          (error) => {
+            res.status(500).json({ error: error.message });
+          }
+        );
 
-          await products.update(
-            {
-              title,
-              description,
-              price,
-              color,
-              size,
-              category,
-              like,
-              dislike,
-              rating,
-              sell,
-              isNew,
-              isTop,
-              imageUrl: poster,
-              imageName: file.originalname,
-              imageType: file.mimetype,
+        await products.update(
+          {
+            title,
+            description,
+            price,
+            color,
+            size,
+            category,
+            like,
+            dislike,
+            rating,
+            sell,
+            isNew,
+            isTop,
+            imageUrl: poster,
+            imageName: file.originalname,
+            imageType: file.mimetype,
+          },
+          {
+            where: {
+              id,
             },
-            {
-              where: {
-                id,
-              },
-            }
-          );
+          }
+        );
 
-          res.status(200).json("resource updated successfuly");
-        } else {
-          res.status(404).json("Not found");
-        }
+        res.status(200).json("resource updated successfuly");
+      } else {
+        res.status(404).json("Not found");
+      }
       // } else {
       //   res.status(500).json("select a new image ");
       // }
