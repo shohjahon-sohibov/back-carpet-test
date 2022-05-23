354a525f583c385061f0dc1e4444e8bf33eb6264
shohjahon-sohibov <sohibov.s@bk.ru>
2022-05-23 17:29:30 +0500

    a

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
+require("dotenv").config()
+
 const PORT = process.env.PORT || 9000
 const SERVERLINK = 'https://fierce-scrubland-73776.herokuapp.com/'
-const elConnections = "postgres://ysdpfncn:RPVTTDPDX0C1Hnh3akO4scf3MbS9rdRq@lallah.db.elephantsql.com/ysdpfncn"
+// const elConnections = "postgres://ysdpfncn:RPVTTDPDX0C1Hnh3akO4scf3MbS9rdRq@lallah.db.elephantsql.com/ysdpfncn"
 
 module.exports = {
     PORT,
     SERVERLINK,
-    elConnections
+    // elConnections,
+    elConnections: env.DB
 }
