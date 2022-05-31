28206ddb690f9d8cab80b9c9b58d07c71a245532
shohjahon-sohibov <sohibov.s@bk.ru>
2022-05-31 17:03:09 +0500

    dssdsdsdss

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json({limit:'50mb'}));
-app.use('/public', express.static('public/uploads'))
+app.use('/public', express.static('public'))
 
 sequelize
   .sync({ force: false })
