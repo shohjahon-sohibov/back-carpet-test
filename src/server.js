af165a61580d673b53f955dceffc46fd9dcf824b
shohjahon-sohibov <sohibov.s@bk.ru>
2022-05-31 14:58:55 +0500

    sa

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(express.json({limit:'50mb'}));
 app.use('/public', express.static('public'))
 
 sequelize
-  .sync({ force: true })
+  .sync({ force: false })
   .then(() => console.log("connected"))
   .catch((err) => console.log(err));
 
