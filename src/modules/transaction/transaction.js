edbc8198fc5480d86c1f602804c5e3961718c549
shohjahon-sohibov <sohibov.s@bk.ru>
2022-06-03 11:39:12 +0500

    ds

--- src/modules/transaction/transaction.js
+++ src/modules/transaction/transaction.js
@@ -179,7 +179,7 @@ module.exports = class HomeController {
 			let x = await Users.increment("user_balance", {
 				by: payment.dataValues.payment_amount,
 				where: {
-					userId: payment.dataValues.user.dataValues.userId,
+					userId: payment.dataValues.user.dataValues.id,
 				},
 			});
 			const date = Date.now();
