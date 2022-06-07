ebd3b7c0abf1465787f28012124beeac5f8204e7
shohjahon-sohibov <sohibov.s@bk.ru>
2022-06-07 17:55:12 +0500

    dsds

--- src/modules/carpet_collections/Prince.js
+++ src/modules/carpet_collections/Prince.js
@@ -1,9 +1,27 @@
-const { Prince_collection } = require('../../model/Prince.collection')
+const { Prince_collection } = require('../../model/Prince.collection');
+const { Carpet_info, Carpet_comments } = require('../../model/model')
 
 module.exports = {
     GET_PRINCE: async (_, res) => {
         try {
-            res.json(await Prince_collection.findAll())
+            res.json(await Prince_collection.findAll({
+                include: [
+                    {
+                      model: Carpet_comments,
+                      attributes: ["id", "body", "carpetCollectionId"],
+                    },
+                    {
+                      model: Carpet_info,
+                      attributes: [
+                        "id",
+                        "size",
+                        "price",
+                        "in_market",
+                        "carpetCollectionId",
+                      ],
+                    },
+                  ],
+            }))
         } catch (e) {
             console.log(e);
         }
