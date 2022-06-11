3c6f66ba5045aa34dc20e7ba25b5e5b4e0085cd8
shohjahon-sohibov <sohibov.s@bk.ru>
2022-06-11 11:56:25 +0500

    dsds

--- src/modules/users/users.js
+++ src/modules/users/users.js
@@ -11,71 +11,71 @@ module.exports = {
       res.status(500).json({ error: error.message });
     }
   },
-  REGISTER: async (req, res) => {
-    try {
-      const { fullname, username, email, phone, password, role, user_balance } = req.body;
-      const newUser = await Users.create({
-        fullname,
-        username,
-        email,
-        phone,
-        password,
-        role,
-        user_balance
-      });
-      const token = signUser({ id: newUser.id, role: newUser.role });
+  // REGISTER: async (req, res) => {
+  //   try {
+  //     const { fullname, username, email, phone, password, role, user_balance } = req.body;
+  //     const newUser = await Users.create({
+  //       fullname,
+  //       username,
+  //       email,
+  //       phone,
+  //       password,
+  //       role,
+  //       user_balance
+  //     });
+  //     const token = signUser({ id: newUser.id, role: newUser.role });
 
-      res.status(201).json(token);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  },
-  LOGIN: async (req, res) => {
-    try {
-      const { username, email, phone, password } = req.body;
+  //     res.status(201).json(token);
+  //   } catch (error) {
+  //     res.status(500).json({ error: error.message });
+  //   }
+  // },
+  // LOGIN: async (req, res) => {
+  //   try {
+  //     const { username, email, phone, password } = req.body;
 
-      const findUserByOption = await Users.findOne({
-        where: {
-          [Op.or]: [{ username }, { email }, { phone }],
-        },
-      });
+  //     const findUserByOption = await Users.findOne({
+  //       where: {
+  //         [Op.or]: [{ username }, { email }, { phone }],
+  //       },
+  //     });
 
-      if (!findUserByOption) {
-        res.status(401).json("User not found");
-      } else {
-        if (!findUserByOption.email) {
-          res.status(401).json("Email is not correct");
-        } else if (!findUserByOption.phone) {
-          res.status(401).json("Phone is not correct");
-        } else if (!findUserByOption.password) {
-          res.status(401).json("Password is not correct");
-        } else {
-          const token = signUser({
-            id: findUserByOption.id,
-            role: findUserByOption.role,
-          });
+  //     if (!findUserByOption) {
+  //       res.status(401).json("User not found");
+  //     } else {
+  //       if (!findUserByOption.email) {
+  //         res.status(401).json("Email is not correct");
+  //       } else if (!findUserByOption.phone) {
+  //         res.status(401).json("Phone is not correct");
+  //       } else if (!findUserByOption.password) {
+  //         res.status(401).json("Password is not correct");
+  //       } else {
+  //         const token = signUser({
+  //           id: findUserByOption.id,
+  //           role: findUserByOption.role,
+  //         });
 
-          res.status(201).json(token);
-        }
-      }
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  },
-  DELETE_ACCOUNT: async (req, res) => {
-    try {
-      const { id, isDelete } = req.body;
-      await Users.findOne(
-        { isDelete },
-        {
-          where: {
-            id,
-          },
-        }
-      );
-      res.status(201).json("resource deleted successfully");
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  },
+  //         res.status(201).json(token);
+  //       }
+  //     }
+  //   } catch (error) {
+  //     res.status(500).json({ error: error.message });
+  //   }
+  // },
+  // DELETE_ACCOUNT: async (req, res) => {
+  //   try {
+  //     const { id, isDelete } = req.body;
+  //     await Users.findOne(
+  //       { isDelete },
+  //       {
+  //         where: {
+  //           id,
+  //         },
+  //       }
+  //     );
+  //     res.status(201).json("resource deleted successfully");
+  //   } catch (error) {
+  //     res.status(500).json({ error: error.message });
+  //   }
+  // },
 };
